892267dfa27e12390276d79d3f60a21990c59708
Neil Rathi <neilrathi17@gmail.com>
2021-07-14 18:10:53 +0530

    second commit

--- src/home.js
+++ src/home.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
+
 const Home = () => {
 
-    const handleClick=()=>
-    {
-        console.log('clicked')
-    }
-     const handleClickagain=(name)=>
-     {
-         console.log("hello"+name)
-     }
+        const [blogs,setBlogs]=useState(
+            [
+                {title:"my new website",body:'lorem ipsum',author:"mario",id:1},
+                {title:"welcome party",body:'lorem ipsum',author:"yoshi",id:2},
+                {title:"web dev top tips",body:'lorem ipsum',author:"luigi",id:3},
+            ]
+        )
+
+ 
+    //  const handleClickagain=(name)=>
+    //  {
+    //      console.log("hello"+name)
+    //      setName('luigi');
+
+    //  }
     return ( 
         <div className="home">
-            <h2>HOMEPAGE</h2>
-            <button onClick={handleClick}>CLICK ME</button>
-            <button onClick={()=>handleClickagain('mario')}>CLICK ME AGAIN</button>
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key={blogs.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            )
+            )}
+           
+            {/* <button onClick={()=>handleClickagain('mario')}>CLICK ME AGAIN</button> */}
+            
         </div>
      );
 }
